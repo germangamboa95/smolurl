4e74699880b01b776d80ca9cce83f3c40090f508
germangamoa95 <germangamboa95@gmail.com>
2021-01-10 02:55:22 -0500

    adding database

--- ormconfig.js
+++ ormconfig.js
@@ -13,16 +13,20 @@ module.exports = [
       "migrationsDir": "src/migration",
       "subscribersDir": "src/subscriber"
     }
-  }, 
+  },
   {
     "name": "production",
-    "type": "sqlite",
-    "database": "database.sqlite",
+    "type": "mysql",
+    host: process.env.DATABASE_URL,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
     "synchronize": false,
     "logging": true,
     "entities": ["build/**/*.js"],
     "migrations": ["build/migration/**/*.js"],
     "subscribers": ["build/subscriber/**/*.js"],
-  
+
   }
 ]
