81f7fe0710e0533a0da9717830900cb28beeabd9
germangamoa95 <germangamboa95@gmail.com>
2021-01-11 02:04:09 -0500

    adding tailwindcss

--- client/app.tsx
+++ client/app.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import { render } from "react-dom";
-
+import "./app.css";
 const app = document.getElementById("app");
 
 render(
-  <div>
+  <div className="">
     <p>Hello World</p>
   </div>,
   app
