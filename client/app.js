4e82881f3ff624009a443e9787ecdff2d1309256
germangamoa95 <germangamboa95@gmail.com>
2021-01-11 01:48:38 -0500

    lets make a huge monolith

---
+++ client/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { render } from "react-dom";
+
+const app = document.getElementById("app");
+
+render(
+  <div>
+    <p>Hello World</p>
+  </div>,
+  app
+);
+
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+
+let mix = require('laravel-mix');
+
+
+
+
+mix.js('client/app.js', 'js').setPublicPath('public');
