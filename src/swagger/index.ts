7c9e155089aa5d6d05fee2514853557eabd39b4b
germangamboa95 <germangamboa95@gmail.com>
2021-02-26 23:42:29 -0500

    remove docs

--- src/swagger/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import swaggerUi from "swagger-ui-express";
-import doc from "./v1.json";
-
-const docs = Router();
-
-docs.use("/docs/api/", swaggerUi.serve, swaggerUi.setup(doc));
-
-export { docs };
